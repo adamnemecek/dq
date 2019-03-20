3c5e1dcd4e3091fdebeed4b955535767dee683a7
Adam Nemecek <adamnemecek@gmail.com>
2019-03-20 15:20:51 -0700

    stuff

--- tests/dual_quaternion.rs
+++ tests/dual_quaternion.rs
@@ -31,7 +31,7 @@ fn test_exp() {
 fn test_pow() {
     let input = dq();
     let e = 1.5f64;
-    let result = input.pow(e).pow(1.0 / e);
+    let result = input.pow(e).pow(1.0/e);
     assert_relative_eq!(input, result, epsilon = 1.0e-7);
 }
 
@@ -39,13 +39,12 @@ fn test_pow() {
 fn test_pow2() {
     let input = dq();
     let e = 9.0f64;
-    let result = input.pow(1.0 / e).pow(e);
+    let result = input.pow(1.0/e).pow(e);
     assert_relative_eq!(input, result, epsilon = 1.0e-7);
 }
 
 #[test]
 fn test_conjugate() {
-
     let a = dq();
     let b = dq2();
 
@@ -57,11 +56,13 @@ fn test_conjugate() {
 
 #[test]
 fn test_slerp() {
+    /// from dq3d import op
+    /// 
     let a = dq();
     let b = dq2();
 
     let result = a.slerp(b, 1.5f64);
-
+    
     let expectedRe = Quaternion::<f64>::new(0.00766191, 0.00959764, 0.0191143, -0.00185473);
     let expectedDu = Quaternion::<f64>::new(0.0256795, 0.0370031, 0.0714485, 0.0113163);
     let expected = DualQuaternion::<f64>::new(expectedRe, expectedDu);
@@ -78,6 +79,7 @@ fn test_inv() {
 }
 
 /// Trigonometry
+
 #[test]
 fn test_cos() {
     let input = dq();
@@ -123,5 +125,5 @@ fn test_tanh() {
 
 // #[test]
 // fn test_rotation() {
-
+    
 // }
